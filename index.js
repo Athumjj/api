0e4ee13bdfe5bbf01093b8ca2e2e7ccf2281917e
Athumjj <melloteam22@gmail.com>
2024-07-28 19:05:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/profile', async (req, res) => {
     try {
         // Load avatar
         const avatar = await loadImage(avatarUrl);
-        ctx.drawImage(avatar, 40, 40, 160, 160);
+        ctx.drawImage(avatar, 40, 40, 150, 150);
         
         // Username
         ctx.font = 'bold 50px "Open Sans"';
