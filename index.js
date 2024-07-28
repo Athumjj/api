7416da995d6b2c7b67650b87d197b28a6e852df5
Athumjj <melloteam22@gmail.com>
2024-07-28 19:10:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/profile', async (req, res) => {
     const ctx = canvas.getContext('2d');
 
     // Background
-    const bgImage = await loadImage("https://cdn.discordapp.com/attachments/1232193462690910220/1267238181422633041/1722203503137.png?ex=66a80f06&is=66a6bd86&hm=2ee13440a861819bbde1bb03668885bb18025b1984490ff2bef621845bfe9944&");
+    const bgImage = await loadImage("https://cdn.discordapp.com/attachments/1232193462690910220/1267242641431724223/1722204575960.png?ex=66a8132d&is=66a6c1ad&hm=6e399f879a798488fdaf717e4832fea0ec47b9dadb0063bcbfc41e7e813a87bf&");
     ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
 
     try {
