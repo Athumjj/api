058884bf6d83457bef3bcd456b78472238db861a
Athumjj <melloteam22@gmail.com>
2024-07-28 14:17:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ const convertToPng = (url) => {
     return url.replace(/(\.jpeg|\.jpg|\.webp)(\?.*)?$/, '.png');
 };
 
+function drawRoundedRect(ctx, x, y, width, height, radius) {
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctx.lineTo(x + width, y + height - radius);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    ctx.lineTo(x + radius, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctx.lineTo(x, y + radius);
+    ctx.quadraticCurveTo(x, y, x + radius, y);
+    ctx.closePath();
+}
+
 app.get('/profile', async (req, res) => {
     let { username, avatarUrl } = req.query;
 
