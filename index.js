b59fe723b52bc5452e09d12f34ec5e964deef0bd
Athumjj <melloteam22@gmail.com>
2024-07-29 20:04:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 
 // Register a specific font
-registerFont(path.join(__dirname, 'Fonte.otf'), { family: 'Open Sans' });
+registerFont(path.join(__dirname, 'Fonte.ttf'), { family: 'Open Sans' });
 
 // Function to clean avatar URL and convert to PNG
 const convertToPng = (url) => {
