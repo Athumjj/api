aff7e8186f303df1b1ea0b7de802b0d33fd11a73
Athumjj <melloteam22@gmail.com>
2024-07-29 20:55:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 
 // Register a specific font
-registerFont(path.join(__dirname, 'fonte.ttf'), { family: 'Open Sans' });
+const fontPath = path.resolve(__dirname, 'fonte.ttf');
+registerFont(fontPath, { family: 'Open Sans' });
 
 // Function to clean avatar URL and convert to PNG
 const convertToPng = (url) => {
@@ -22,23 +23,26 @@ app.get('/profile', async (req, res) => {
     // Convert avatar URL to PNG
     avatarUrl = convertToPng(avatarUrl);
 
-    const canvas = createCanvas(700, 250);
+    const canvas = createCanvas(1280, 720);
     const ctx = canvas.getContext('2d');
 
     // Background
-    ctx.fillStyle = '#7289da';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    const bgImage = await loadImage("https://cdn.discordapp.com/attachments/1232193462690910220/1267242641431724223/1722204575960.png?ex=66a8132d&is=66a6c1ad&hm=6e399f879a798488fdaf717e4832fea0ec47b9dadb0063bcbfc41e7e813a87bf&");
+    ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
 
     try {
         // Load avatar
         const avatar = await loadImage(avatarUrl);
-        ctx.drawImage(avatar, 25, 25, 200, 200);
+        ctx.drawImage(avatar, 70, 35, 150, 150);
 
+        const avatarMold = await loadImage("https://cdn.discordapp.com/attachments/1243775486514040934/1267246776709156928/281_Sem_Titulo_20240728192414.png?ex=66a81707&is=66a6c587&hm=1d177313e8983b8738ab57dc18b948d49876b1d07d2b9d0770b8f0029a6def48&");
+        ctx.drawImage(avatarMold, 0, 0, canvas.width, canvas.height);
+        
         // Username
-        ctx.font = 'bold 40px "Open Sans"';
+        ctx.font = 'bold 50px "Open Sans"';
         ctx.fillStyle = '#ffffff';
         ctx.fillText(username, 250, 125);
-
+        
         const buffer = canvas.toBuffer('image/png');
         res.set('Content-Type', 'image/png');
         res.send(buffer);
